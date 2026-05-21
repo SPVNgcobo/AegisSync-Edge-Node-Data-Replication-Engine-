d68d0dadeb91cca87e10f65e9e9eb7009ecfb53e
Snethemba Ngcobo <120355862+SPVNgcobo@users.noreply.github.com>
2026-05-21 17:03:50 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,3 +7,17 @@ builder.Services.AddSingleton<IGrpcSyncClient, GrpcSyncClient>();
 
 // Start the FSM Engine
 builder.Services.AddHostedService<FsmWorker>();
+
+// Register the actual repository instead of the mock
+builder.Services.AddSingleton<ISqliteRepository, SqliteRepository>();
+
+var host = builder.Build();
+
+// Initialize the database before starting the workers
+using (var scope = host.Services.CreateScope())
+{
+    var repo = scope.ServiceProvider.GetRequiredService<ISqliteRepository>();
+    await repo.InitializeDatabaseAsync();
+}
+
+host.Run();
